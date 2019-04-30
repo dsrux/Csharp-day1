b295619fafe17407a5101987c49adf5a8c0c16ec
dsrux <dsrux777777@gmail.com>
2019-04-29 19:33:50 -0600

    initialize

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace hwapp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+      Console.WriteLine("Hello User, what is your name?");
+      string name = Console.ReadLine();
+      Console.WriteLine("How old are you?");
+      string age = Console.ReadLine();
+      //   Console.WriteLine("Hello" + name + ". It sounds like "+ name + "is " + age + "years old");
+      //   Console.WriteLine("Hello {0}. It sounds liek {0} is {1} years old", name, age);
+      Console.WriteLine($"Hello {name}. It sounds like {name} is {age} years old.");
+
+      int myNum = 30 * 82;
+      myNum = myNum * 2;
+      myNum *= 2;
+
+      Console.WriteLine(15 % 12);
+      bool isAwesome = true;
+
+
+
+    }
+  }
+}
